44464c27df3b1602bf5bb7000c7d980f577f6ca6
eminamvc <92106170+eminamvc@users.noreply.github.com>
2025-12-05 15:04:28 +0100

    Create AgedBrieUpdater.java

---
+++ src/main/java/gildedrose/AgedBrieUpdater.java
@@ -0,0 +1,16 @@
+package gildedrose;
+
+public class AgedBrieUpdater implements ItemUpdater {
+
+    @Override
+    public void update(Item item) {
+        item.sellIn--;
+
+        if (item.quality < 50) {
+            item.quality++;
+            if (item.sellIn < 0 && item.quality < 50) {
+                item.quality++;
+            }
+        }
+    }
+}
